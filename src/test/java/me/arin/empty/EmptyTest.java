bb44a87f2ba3cdcd52ba97ff1a79458045b54444
Arin Sarkissian <arins@uber.com>
2017-12-08 09:54:51 -0800

    bla

--- src/test/java/me/arin/empty/EmptyTest.java
+++ src/test/java/me/arin/empty/EmptyTest.java
@@ -1,6 +1,12 @@
-import static org.junit.Assert.*;
+package me.arin.empty;
+
+import org.junit.Assert;
+import org.junit.Test;
 
 
 public class EmptyTest {
-
+  @Test
+  public void testNothing() {
+    Assert.assertTrue(true);
+  }
 }
